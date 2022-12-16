4d3b1c74683dc44091fca0fca092b8aa156bf1a5
Hamid Ashhad <hamidashhad11@gmail.com>
2022-12-16 23:42:23 +0500

    basic controller for application

--- src/main/java/com/hamid/spring/springSecurity/SpringSecurityApplication.java
+++ src/main/java/com/hamid/spring/springSecurity/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package com.hamid.spring.springSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hamid.spring.springSecurity.controller")
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
