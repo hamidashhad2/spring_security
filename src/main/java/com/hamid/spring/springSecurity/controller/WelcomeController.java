18c74e4b76018e43872cc606806557f23de97515
Hamid Ashhad <hamidashhad11@gmail.com>
2022-12-17 01:07:00 +0500

    added login creds

--- src/main/java/com/hamid/spring/springSecurity/controller/WelcomeController.java
+++ src/main/java/com/hamid/spring/springSecurity/controller/WelcomeController.java
@@ -8,6 +8,6 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to spring application without security";
+        return "Welcome to spring application security";
     }
 }
